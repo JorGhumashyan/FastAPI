856cda89cf73eecbb0daf7f30f3252c5e9c86d98
JorGhumashyan <102811285+JorGhumashyan@users.noreply.github.com>
2024-06-17 21:56:57 +0400

    done 3

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
 
 
-
+####################################################### 
 
 
 from fastapi import FastAPI, File, UploadFile
@@ -84,3 +84,78 @@ async def upload_image(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+# ########################################################################### 
+
+
+
+
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+class Task(BaseModel):
+    id: int
+    title: str
+    description: str
+
+
+tasks_db = []
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI To-Do List Application!"}
+
+@app.get("/tasks", response_model=List[Task])
+def get_tasks():
+    return tasks_db
+
+@app.post("/tasks", response_model=Task)
+def create_task(task: Task):
+    for existing_task in tasks_db:
+        if existing_task.id == task.id:
+            raise HTTPException(status_code=400, detail="Task ID already exists")
+    
+    tasks_db.append(task)
+    return task
+
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: int):
+    for task in tasks_db:
+        if task.id == task_id:
+            tasks_db.remove(task)
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+# uvicorn main:app --reload (how to run)
+#  curl -X 'GET' \
+# 'http://127.0.0.1:8000/tasks' \
+# -H 'accept: application/json'   (get all tasks)
+
+
+# curl -X 'POST' \
+# 'http://127.0.0.1:8000/tasks' \
+#-H 'accept: application/json' \
+#-H 'Content-Type: application/json' \
+#-d '{
+#"id": 1,
+#"title": "Buy groceries",
+#"description": "Milk, Bread, Cheese"
+#}'     (create new list)
+
+
+
+
+
